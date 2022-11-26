5fc46250824ee4ac7ec7e3c3038ea68eb8fc4da7
ShreeChavan9595 <118918706+ShreeChavan9595@users.noreply.github.com>
2022-11-26 17:59:38 +0530

    Update TestClass1.java

--- gitpro/src/test/java/pack1/TestClass1.java
+++ gitpro/src/test/java/pack1/TestClass1.java
@@ -5,15 +5,7 @@ public class TestClass1 {
 	public static void main(String[] args) {
 		
 		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
+		
 		
 		
 		
