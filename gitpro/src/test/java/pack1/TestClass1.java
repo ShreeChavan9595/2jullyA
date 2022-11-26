729c79dae594f1b7cc2e10c5ca455e14512bfd7f
Admin <Admin@DESKTOP-138AOJ8>
2022-11-26 17:32:26 +0530

    First Commit

---
+++ gitpro/src/test/java/pack1/TestClass1.java
@@ -0,0 +1,25 @@
+package pack1;
+
+public class TestClass1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		
+		
+		
+		
+	}
+	
+	
+
+}
